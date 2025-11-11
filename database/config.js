b06a0f2a853e89a9ae5e123cfb467ee01186b117
masarsah <ss630883@gmail.com>
2025-11-11 04:18:23 +0300

    name

--- database/config.js
+++ database/config.js
@@ -11,11 +11,11 @@ const db = pgp({
 });
 
   const listener = pgp({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'chaten',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '12345678',
+  host: 'localhost',
+  port: 5432,
+  database:  'chaten',
+  user: 'postgres',
+  password:  '12345678',
 });
 
 export { db, listener };
